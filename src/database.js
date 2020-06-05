9b607788bd50aea7a6388ffddc14a158929fa0e0
JJ <justinj1995@outlook.com>
2020-06-05 10:03:22 -0600

    added function to filter out duplicate search results

--- src/database.js
+++ src/database.js
@@ -11,7 +11,8 @@ export default class DataBase {
 
 DataBase.prototype.getResults = function (cityNameIn) {
   let regEx = new RegExp(cityNameIn, "i");
-  this.cityResults = this.cityNames.filter((city) => regEx.test(city));
+  let initialResults = this.cityNames.filter((city) => regEx.test(city));
+  this.cityResults = this.filterAmbiguous(initialResults);
   return this.cityResults;
 };
 DataBase.prototype.getResultsAsIds = function () {
@@ -27,4 +28,17 @@ DataBase.prototype.getResultsAsIds = function () {
     idsIndex++; //to avoid potential ambiguity
   }
   return idResults;
+};
+
+DataBase.prototype.filterAmbiguous = function (arrayIn) {
+  let arrayOut = [];
+  for (let index = 0; index < arrayIn.length; index++) {
+    let a_i = arrayIn[index];
+    if (!arrayOut.includes(a_i)) {
+      arrayOut.push(a_i); //not already in array, so put it in there
+    } else {
+      continue;
+    }
+  }
+  return arrayOut;
 };
